1d3b813c6d0b00676eba3120837a922e6ada12cd
whitgroves <whitney.groves@gmail.com>
2023-10-16 22:33:25 -0500

    tutorial part 4/13

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, request, url_for, flash, redirect
 from markupsafe import escape # renders URL variable as text to prevent XSS attacks
 import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '0b6f5b665fbc26e72c6cffb7de43022196d437ad94b1e8a2' # os.urandom(24).hex()
+
+messages = [{'title': f'Message {i}', 'content': f'Message {i} content'} for i in range(3)]
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -15,7 +18,7 @@ def internal_error(error):
 @app.route('/')
 @app.route('/index/')
 def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('index.html', messages=messages, utc_dt=datetime.datetime.utcnow())
 
 @app.route('/about/')
 def about():
@@ -50,10 +53,10 @@ def comments():
 @app.route('/messages/<int:idx>/')
 def message(idx):
     app.logger.info('Building messages list...')
-    messages = [f'Message {n}' for n in range(10)]
+    _messages = [f'Message {n}' for n in range(10)]
     try:
         app.logger.debug(f'Fetching message at index: {idx}')
-        return render_template('message.html', message=messages[idx])
+        return render_template('message.html', message=_messages[idx])
     except IndexError:
         app.logger.error(f'No message found at index: {idx}')
         abort(404)
@@ -61,3 +64,18 @@ def message(idx):
 @app.route('/500/')
 def error500():
     abort(500)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('hello'))
+            
+    return render_template('create.html')
